04b7168c3a6b06403a212116e60c356653c607fb
dirak <dirak.social@gmail.com>
2019-01-03 00:06:44 -0500

    absolutely garbage but works

--- weeks/1/app.js
+++ weeks/1/app.js
@@ -3,10 +3,10 @@ var game = new Phaser.Game({
 	backgroundColor: '#708090',
 	scene: {
 		preload: function() {
-
+			this.load.atlas('atlas', 'assets/atlas.png','assets/atlas.json');
 		},
 		create: function() {
-
+			this.add.sprite(0, 0, 'atlas', '1').setOrigin(0);
 		},
 		update: function() {
 
