9fd01e4f07ad1fc7496dd0abe9f69471b9745e6b
dirak <dirak.social@gmail.com>
2019-01-05 21:32:01 -0500

    forward guide snapping

--- 2/app.js
+++ 2/app.js
@@ -1,6 +1,8 @@
 var tile_width = 32;
 var tile_height = 32;
 
+const DEBUG = 1;
+
 let snap_to_grid = (x, y) => [Math.floor(x/tile_width)*tile_width, Math.floor(y/tile_height)*tile_height];
 
 let possible_directions = {
@@ -8,17 +10,33 @@ let possible_directions = {
 	'right': ['right', 'cw_down', 'cc_up'],
 	'down': ['down', 'cw_left', 'cc_right'],
 	'left': ['left', 'cw_up', 'cc_down'],
-	'none': ['up', 'right', 'down', 'left']
+	'none': ['up', 'right', 'down', 'left'],
+	'cc_up': ['up', 'cc_left', 'cw_right'],
+	'cw_up': ['up', 'cc_left', 'cw_right'],
+	'cc_right': ['right', 'cw_down', 'cc_up'],
+	'cw_right': ['right', 'cw_down', 'cc_up'],
+	'cc_down': ['down', 'cw_left', 'cc_right'],
+	'cw_down': ['down', 'cw_left', 'cc_right'],
+	'cc_left': ['left', 'cw_up', 'cc_down'],
+	'cw_left': ['left', 'cw_up', 'cc_down'],
 };
 
 let guide_position = (direction, x, y) => {
 	switch(direction) {
+		case 'cc_up':
+		case 'cw_up':
 		case 'up':
 			return [x, y-tile_height];
+		case 'cc_right':
+		case 'cw_right':
 		case 'right':
 			return [x+tile_width, y];
+		case 'cc_down':
+		case 'cw_down':
 		case 'down':
 			return [x, y+tile_height];
+		case 'cc_left':
+		case 'cw_left':
 		case 'left':
 			return [x-tile_width, y];
 	}
@@ -61,20 +79,30 @@ var game = new Phaser.Game({
 					case 1:
 						[x, y] = snap_to_grid(pointer.x, pointer.y);
 						let direction = possible_directions[current_connection][current_direction];
-						let tmp = this.add.sprite(x, y, 'atlas', direction).setOrigin(0);
-						tmp.connection = current_connection;
-						this.items.add(tmp);
-
-						let tmp_guide = this.add.sprite(...guide_position(direction, x, y), 'guide').setOrigin(0);
-						tmp_guide.connection = direction;
-						tmp_guide.setInteractive();
-						tmp_guide.on('pointerover', () => {
-							current_connection = tmp_guide.connection;
-						});
-						tmp_guide.on('pointerout', () => {
+						//add the new item
+						let item = this.add.sprite(x, y, 'atlas', direction).setOrigin(0);
+						item.connection = current_connection;
+						this.items.add(item);
+						//
+						//add the guides for the new item
+						let forward_guide = this.add.sprite(...guide_position(direction, x, y), 'guide')
+							.setOrigin(0)
+							.setTintFill(0x45FF45)
+							.setAlpha(DEBUG);
+						forward_guide.connection = direction;
+						forward_guide.setInteractive();
+						forward_guide.on('pointerout', () => {
 							current_connection = 'none';
-						})
-						this.guides.add(tmp_guide);
+							current_direction = 0;
+							this.selection.setFrame(possible_directions[current_connection][current_direction]);
+						});
+						forward_guide.on('pointerover', () => {
+							current_connection = forward_guide.connection;
+							current_direction = 0;
+							this.selection.setFrame(possible_directions[current_connection][current_direction]);
+						});
+						this.guides.add(forward_guide);
+						//
 						break;
 					case 2:
 
